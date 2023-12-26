3b01055b3083399b9a790753c6cfb379edc8751c
Pulkit Saxena <77536409+Pulkit-s21@users.noreply.github.com>
2023-12-26 22:22:39 +0530

    "Alt tag added to image"

--- _blogPreview/src/components/Image.jsx
+++ _blogPreview/src/components/Image.jsx
@@ -1,5 +1,5 @@
 import Article from "../assets/Article.svg"
 
 export const Image = () => {
-  return <img src={Article} className="rounded-xl" />
+  return <img src={Article} className="rounded-xl" alt="Blog Image"/>
 }
